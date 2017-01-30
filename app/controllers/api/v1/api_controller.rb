b3dd004b9f708ecebbccf74b7b891d23d358e474
Tony Rail <tmamacos@gmail.com>
2017-01-30 22:08:00 +0200

    Initial commit

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,6 @@
+# app/controllers/api/v1/api_controller.rb
+module Api::V1
+  class ApiController < ApplicationController
+    # Generic API stuff here
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: 'api' do
+    namespace :v1 do
+      resources :users
+    end
+  end
+end
